151d28a00d17cae13a38fe7d247135c370ee992b
Mohammad <mfelfelani72@gmail.com>
2025-09-29 15:30:16 +0000

    update

---
+++ dist/config/language.js
@@ -0,0 +1,4 @@
+export const languages = {
+    en: { dir: "ltr", name: "English" },
+    fa: { dir: "rtl", name: "فارسی" },
+};
---
+++ dist/dictionaries/index.d.ts
@@ -0,0 +1,15 @@
+export declare function getDictionary(lang: "en" | "fa"): Promise<{
+    login: {
+        title: string;
+    };
+    register: {
+        title: string;
+    };
+} | {
+    login: {
+        title: string;
+    };
+    register: {
+        title: string;
+    };
+}>;
